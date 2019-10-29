f6f105216dc9746aaa8aac92c15708ff77602f6a
javit <javit@LAPTOP-VDMD61LM>
2019-10-29 16:37:45 +0100

    metodo de guardar unidad en la bd

--- DeustoDungeon/src/gestorDB/GestorDB.java
+++ DeustoDungeon/src/gestorDB/GestorDB.java
@@ -24,10 +24,16 @@ public class GestorDB {
 	}
 	
 	public void guardarUnidad(Unidad unidad) throws SQLException {
-		String sql = "INSERT INTO unidad (nombre, descripcion, nivel, atkFis, arkMag, vida, defFis, defMag) VALUES(?,?,?,?,?,?,?,?,?)";
+		String sql = "INSERT INTO unidad (nombre, descripcion, nivel, atkFis, atkMag, vida, defFis, defMag) VALUES(?,?,?,?,?,?,?,?,?)";
 		PreparedStatement stmt = conn.prepareStatement(sql);
-		stmt.setString(1, unidad.getNom());
-		
+		stmt.setString(1,unidad.getNom());
+		stmt.setString(2,unidad.getDescripcion());
+		stmt.setInt(3,unidad.getNivel());
+		stmt.setInt(4,unidad.getAtkFis());
+		stmt.setInt(5,unidad.getAtkMag());
+		stmt.setInt(6,unidad.getVida());
+		stmt.setInt(7,unidad.getDefFis());
+		stmt.setInt(8,unidad.getDefMag());
 	}
 	static void main(String[] args) {
 		// TODO Auto-generated method stub

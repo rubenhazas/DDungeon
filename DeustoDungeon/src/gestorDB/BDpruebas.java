dd68889d319f10897b156eec27cac5eb4a171be2
javit <javit@LAPTOP-VDMD61LM>
2019-10-29 16:59:08 +0100

    prueba de introducir datos a la bd

--- DeustoDungeon/src/gestorDB/BDpruebas.java
+++ DeustoDungeon/src/gestorDB/BDpruebas.java
@@ -1,10 +1,21 @@
 package gestorDB;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import datos.Unidad;
 
 public class BDpruebas {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static GestorDB miDB;
+	public static void main(String[] args)throws SQLException, ClassNotFoundException {
+		
+		miDB= new GestorDB();
+		miDB.conectar();
+		Unidad unidad;
+		unidad= new Unidad();
+		miDB.guardarUnidad(unidad);
+		
 		
 		
 	}

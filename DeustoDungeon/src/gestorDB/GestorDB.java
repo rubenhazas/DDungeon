32aec4642b98b184cbbb563cbe2eadf8e2031cfc
javit <javit@10.164.58.154>
2019-10-29 12:33:55 +0100

    primeros metodos de la base de datos

--- DeustoDungeon/src/gestorDB/GestorDB.java
+++ DeustoDungeon/src/gestorDB/GestorDB.java
@@ -1,11 +1,34 @@
 package gestorDB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import datos.Unidad;
+
 public class GestorDB {
 
 
+	private Connection conn;
 	
+	public void conectar() throws ClassNotFoundException, SQLException {
+		Class.forName("org.sqlite.JDBC");
+		
+		conn= DriverManager.getConnection("jdbc:sqlite:DB/DB.db");
+	}
 	
+	public void desconectar() throws SQLException{
+		
+		conn.close();
+	}
 	
+	public void guardarUnidad(Unidad unidad) throws SQLException {
+		String sql = "INSERT INTO unidad (nombre, descripcion, nivel, atkFis, arkMag, vida, defFis, defMag) VALUES(?,?,?,?,?,?,?,?,?)";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, unidad.getNom());
+		
+	}
 	static void main(String[] args) {
 		// TODO Auto-generated method stub
 

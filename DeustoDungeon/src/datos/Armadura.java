c488afb0143e93488cfbdfda5ef16ef5ed518394
javit <javit@10.164.51.120>
2019-11-05 10:53:15 +0100

    metodo para añadir cascos a la bd

--- DeustoDungeon/src/datos/Armadura.java
+++ DeustoDungeon/src/datos/Armadura.java
@@ -20,6 +20,46 @@ public class Armadura {
 	public Armadura() {
 		
 	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getBuffVida() {
+		return buffVida;
+	}
+
+	public void setBuffVida(int buffVida) {
+		this.buffVida = buffVida;
+	}
+
+	public int getBuffDefFis() {
+		return buffDefFis;
+	}
+
+	public void setBuffDefFis(int buffDefFis) {
+		this.buffDefFis = buffDefFis;
+	}
+
+	public int getBuffDefMag() {
+		return buffDefMag;
+	}
+
+	public void setBuffDefMag(int buffDefMag) {
+		this.buffDefMag = buffDefMag;
+	}
 	
 	
 	
--- DeustoDungeon/src/gestorDB/GestorDB.java
+++ DeustoDungeon/src/gestorDB/GestorDB.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import datos.Casco;
 import datos.Unidad;
 
 public class GestorDB {
@@ -40,6 +41,17 @@ public class GestorDB {
 		System.out.println("unidad guardada");
 		
 	}
-	
+	public void guardarCasco(Casco casco)throws SQLException{
+		String sql = "INSERT INTO casco (nombre, descripcion, buffVida, buffDefFis, buffDefMag) VALUES(?,?,?,?,?)";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, casco.getNom());
+		stmt.setString(2, casco.getDescripcion());
+		stmt.setInt(3, casco.getBuffVida());
+		stmt.setInt(4, casco.getBuffDefFis());
+		stmt.setInt(5, casco.getBuffDefMag());
+		
+		stmt.executeUpdate();
+		System.out.println("casco guardado");
+	}
 
 }

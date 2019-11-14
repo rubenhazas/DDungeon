9045a8260503b38328331e06cf9264754d05772b
Rubén <Rubn@10.164.52.77>
2019-11-14 16:28:17 +0100

    ventana personaje

---
+++ DeustoDungeon/src/ui/VentanaPersonaje.java
@@ -0,0 +1,28 @@
+package ui;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.*;
+
+import datos.Arma;
+import datos.Casco;
+import datos.Pechera;
+
+public class VentanaPersonaje extends JFrame {
+	
+	public JLabel nom = new JLabel("Nombre");
+	public JLabel des = new JLabel("Descripcion");
+	public JLabel nivel = new JLabel("Nivel");
+	public JLabel atkFis = new JLabel("Ataque Fisico");
+	public JLabel atkMag = new JLabel("Nivel");
+	public JLabel vida = new JLabel("Nivel");
+	public JLabel defFis = new JLabel("Nivel");
+	public JLabel defMag = new JLabel("Nivel");
+	public JLabel pasivaRacial = new JLabel("Nivel");
+	
+	
+	
+
+}

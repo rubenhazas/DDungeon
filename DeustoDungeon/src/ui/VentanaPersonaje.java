04b66b75101e77854b747d3a196d7918e4fa521d
Rubén <Rubn@10.164.52.77>
2019-11-14 16:38:48 +0100

    v personajes+

--- DeustoDungeon/src/ui/VentanaPersonaje.java
+++ DeustoDungeon/src/ui/VentanaPersonaje.java
@@ -11,16 +11,26 @@ import datos.Casco;
 import datos.Pechera;
 
 public class VentanaPersonaje extends JFrame {
-	
+
 	public JLabel nom = new JLabel("Nombre");
 	public JLabel des = new JLabel("Descripcion");
 	public JLabel nivel = new JLabel("Nivel");
 	public JLabel atkFis = new JLabel("Ataque Fisico");
-	public JLabel atkMag = new JLabel("Nivel");
-	public JLabel vida = new JLabel("Nivel");
-	public JLabel defFis = new JLabel("Nivel");
-	public JLabel defMag = new JLabel("Nivel");
-	public JLabel pasivaRacial = new JLabel("Nivel");
+	public JLabel atkMag = new JLabel("Ataque Magico");
+	public JLabel vida = new JLabel("Vida");
+	public JLabel defFis = new JLabel("Defensa Fisica");
+	public JLabel defMag = new JLabel("Defensa Magica");
+	public JLabel pasivaRacial = new JLabel("Pasiva Racial");
+	
+	private JPanel contentPane;
+	
+	public VentanaPersonaje(VentanaMenu v) {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(0, 0, 400, 700);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		contentPane = new JPanel();
+	}
 	
 	
 	

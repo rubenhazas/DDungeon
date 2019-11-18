0646ddc70e8a880aca2f2d73d1cd48baf85fcc57
Rubén <Rubn@192.168.1.189>
2019-11-18 18:19:27 +0100

    cambios ventana pj

--- DeustoDungeon/src/ui/VentanaPersonaje.java
+++ DeustoDungeon/src/ui/VentanaPersonaje.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.*;
 
+import com.sun.java.util.jar.pack.Attribute.Layout;
+
 import datos.Arma;
 import datos.Casco;
 import datos.Pechera;
@@ -22,6 +24,15 @@ public class VentanaPersonaje extends JFrame {
 	public JLabel defMag = new JLabel("Defensa Magica");
 	public JLabel pasivaRacial = new JLabel("Pasiva Racial");
 	
+	public JTextField nomt = new JTextField("Nombre");
+	public JTextField dest = new JTextField("Descripcion");
+	public JTextField nivelt = new JTextField("Nivel");
+	public JTextField atkFist = new JTextField("Ataque Fisico");
+	public JTextField atkMagt = new JTextField("Vida");
+	public JTextField defFist = new JTextField("Defensa Fisica");
+	public JTextField defMagt = new JTextField("Defensa Magica");
+	public JTextField pasivaRacialt = new JTextField("Pasiva Racial");
+	
 	private JPanel contentPane;
 	
 	public VentanaPersonaje(VentanaMenu v) {
@@ -31,6 +42,24 @@ public class VentanaPersonaje extends JFrame {
 		setLocationRelativeTo(null);
 		setResizable(false);
 		contentPane = new JPanel();
+		contentPane.setBounds(20, 20, 300, 600);
+		contentPane.setLayout(new GridLayout(9,2));
+		contentPane.add(nom);
+		contentPane.add(nomt);
+		contentPane.add(des);
+		contentPane.add(dest);
+		contentPane.add(nivel);
+		contentPane.add(nivelt);
+		contentPane.add(atkFis);
+		contentPane.add(atkFist);
+		contentPane.add(atkMag);
+		contentPane.add(atkMagt);
+		contentPane.add(defFis);
+		contentPane.add(defFist);
+		contentPane.add(defMag);
+		contentPane.add(defMagt);
+		contentPane.add(nom);
+		contentPane.add(nom);
 		
 	}
 	

306d1195c87d2ceaf9d6d8f1eb894e15a0a8a137
javit <javitxu.gonzalez@opendeusto.es>
2020-01-05 12:52:20 +0100

    asdc

--- DeustoDungeon/src/datos/Aliado.java
+++ DeustoDungeon/src/datos/Aliado.java
@@ -6,7 +6,7 @@ public class Aliado extends Unidad{
 	public Casco casco;
 	public Pechera pechera;
 
-	public Aliado(String nom, String descripcion,int nivel, int atkFis, int atkMag, int vida, int defFis, int defMag,String pasivaRacial, Arma arma, Casco casco, Pechera pechera) {
+	public Aliado(String nom, String descripcion,int nivel, int atkFis, int atkMag, int vida, int defFis, int defMag, Arma arma, Casco casco, Pechera pechera) {
 		super(nom,descripcion,nivel,atkFis,atkMag,vida,defFis,defMag);
 		this.arma= arma;
 		this.casco=casco;

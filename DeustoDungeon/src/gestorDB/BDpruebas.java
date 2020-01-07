bdf48126b12054376d8fe10948087bbd9be556fa
javit <javitxu.gonzalez@opendeusto.es>
2020-01-07 16:01:55 +0100

    adasa

--- DeustoDungeon/src/gestorDB/BDpruebas.java
+++ DeustoDungeon/src/gestorDB/BDpruebas.java
@@ -12,13 +12,13 @@ public class BDpruebas {
 
 	static GestorDB miDB;
 	public static void main(String[] args)throws SQLException, ClassNotFoundException {
-		miDB= new GestorDB();
-		miDB.conectar();
-		Armadura casco = new Armadura("Pechera de hierro","pechera de hierro",40,5,3);
+		//miDB= new GestorDB();
+		//miDB.conectar();
+		//Armadura casco = new Armadura("Pechera de hierro","pechera de hierro",40,5,3);
 		
-		miDB.guardarArmadura(casco,"pechera");
-		System.out.println("guardado");
-		miDB.desconectar();
+		//miDB.guardarArmadura(casco,"pechera");
+		//System.out.println("guardado");
+		//miDB.desconectar();
 		
 	}
 

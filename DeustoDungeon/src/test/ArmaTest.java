e4ec49113f6992cd64933f8ae8b6d588ed0bdd2d
Propietario <Propietario@Javitxu>
2020-01-11 15:34:41 +0100

    test de la clase arma

---
+++ DeustoDungeon/src/test/ArmaTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import datos.Arma;
+
+public class ArmaTest {
+	private Arma a ; 
+	
+	@Before
+	public void setUp() {
+	a = new Arma();
+	a.setNom("espada magica");
+	a.setBuffAtkFis(10);
+	}
+	@Test
+	public void testGetNom() {
+		assertEquals("espada magica", a.getNom());
+	}
+
+	@Test
+	public void testGetBuffAtkFis() {
+		assertEquals(10, a.getBuffAtkFis());
+	}
+}

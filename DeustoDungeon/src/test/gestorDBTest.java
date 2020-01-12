35437398debbbac9ce3291fc593360197712677c
Propietario <Propietario@Javitxu>
2020-01-12 18:20:43 +0100

    test de la base de datos

---
+++ DeustoDungeon/src/test/gestorDBTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import gestorDB.GestorDB;
+
+public class gestorDBTest {
+	private GestorDB db;
+	@Before
+	public void setUp() throws SQLException, ClassNotFoundException {
+		db = new GestorDB();
+		db.conectar();
+		
+	}
+	@Test
+	public void testObtenerAliado() throws SQLException {
+		
+		assertEquals("Javi", db.obtenerAliado("Javi").getNom());
+	}
+
+}
--- DeustoDungeon/src/ui/VentanaMenu.java
+++ DeustoDungeon/src/ui/VentanaMenu.java
@@ -10,18 +10,17 @@ import javax.swing.JPanel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+
+
 import java.sql.Connection;
 
-import javax.imageio.ImageIO;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 import gestorDB.GestorDB;
 import java.awt.Font;
-import java.awt.Image;
+
 import java.awt.Color;
 
 public class VentanaMenu extends JFrame {
